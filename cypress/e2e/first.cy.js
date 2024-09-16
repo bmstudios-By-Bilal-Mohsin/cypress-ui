a2cdc5d984a711663ac973268880cc5410db8474
bilalmohsinqa <bmohsin@tcpsoftware.com>
2024-09-16 20:07:37 +0500

    abc

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -2,9 +2,17 @@ context('Launch my app', ()=> {
 
     specify('app testing', ()=>{
         
-        cy.visit('https://www.google.com/')
-        cy.get('#APjFqb').click().clear().type('Hi{backspace}')
+        cy.visit('https://parabank.parasoft.com/parabank/index.htm')
+        cy.get(':nth-child(5) > .button').click()
+        cy.get('.error').should('exist')
+        cy.wait(100)
+        cy.get('.leftmenu > :nth-child(6) > a').click()
+        cy.get('#loanProvider').select('JMS')
 
-        
+  
+
+        // cy.get('[data-test="username"]').click().type('standard_user')
+        // cy.get('[data-test="password"]').click().type('secret_sauce')
+        // cy.get('[data-test="login-button"]').click()
     })
 })
